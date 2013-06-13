9e9db1f03f799767173054e1d3bb1619a864ecea
Rob Miller <rcm@mit.edu>
2013-06-13 09:09:05 -0400

    started Thursday code

---
+++ web-inspector.js
@@ -0,0 +1,62 @@
+/**
+ * A simple web inspector.
+ *
+ * Intended to be a singleton: this only exists once per page, so it
+ * attaches itself to the BODY element.
+ */
+var Inspector = function($) {
+  exports = {};
+
+  // The root element of the inspector.
+  var root = null;
+
+  /*
+   * Construct the UI
+   */
+  exports.initialize = function() {
+    root = $("<div class='inspector'></div>").appendTo($('body'));
+  };
+  
+  return exports;
+};
+
+/*****************************************************************************
+ * Boot up the web inspector!
+ *
+ * This will enable you to COPY AND PASTE this entire file into any web page
+ * to inspect it.
+ *
+ * XXX TODO!
+ *  Change the CSS link below to point to the full URL of your CSS file!
+ *
+ *  You shouldn't need to touch anything else below.
+ *
+ *****************************************************************************/
+(function() {
+    var createInspector = function() {
+      window.inspector = Inspector(jQuery);
+      window.inspector.initialize();
+    }
+
+    // Add the CSS file to the HEAD
+    var css = document.createElement('link');
+    css.setAttribute('rel', 'stylesheet');
+    css.setAttribute('type', 'text/css');
+    css.setAttribute('href', 'web-inspector.css'); // XXX TODO CHANGEME!!
+    document.head.appendChild(css);
+
+    if ('jQuery' in window) {
+      createInspector(window.jQuery);
+    } else {
+      // Add jQuery to the HEAD and then start polling to see when it is there
+      var scr = document.createElement('script');
+      scr.setAttribute('src','http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+      document.head.appendChild(scr);
+      var t = setInterval(function() {
+        if ('jQuery' in window) {
+          clearInterval(t); // Stop polling 
+          createInspector();
+        }
+      }, 50);
+    }
+})();
