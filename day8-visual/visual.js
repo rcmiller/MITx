0c41b489ae02881a59829ed38290260217624693
Rob Miller <rcm@mit.edu>
2013-06-19 10:52:35 -0400

    bar chart is now vertical

--- day8-visual/visual.js
+++ day8-visual/visual.js
@@ -1,29 +1,28 @@
 var data = [0, 4, 8, 15, 160, 40, 42];
 
-// var chart = $("<div></div>").addClass("chart");
-// $(".chart-container").append(chart);
+var chartWidth = 300; // pixels
+var chartHeight = 300; // pixels
 
-// data.forEach(function(d) { chart.append($("<div></div>").addClass("bar").css("width", d+"em").text(d)); })
-
+var xScale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0, chartWidth]);
+var yScale = d3.scale.linear().domain([0, d3.max(data)]).range([0, chartHeight]);
 
 var chart = d3.select(".chart-container")
               .append("svg")
-              .attr("class", "chart");
-var xScale = d3.scale.linear().domain([0, d3.max(data)+5]).range(["0%","100%"]);
-var chartHeight = 140; // pixels
-var yScale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0, chartHeight]);
+              .attr("class", "chart")
+              .attr("height", chartHeight)
+              .attr("width", chartWidth);
 
 chart.selectAll("rect").data(data)
   .enter().append('rect')
-  .attr('width', xScale)
-  .attr('y', function (d, i) { return yScale(i); })
-  .attr('height', "20px");
+  .attr('x', function (d, i) { return xScale(i); })
+  .attr('y', function(d) { return chartHeight - yScale(d); })
+  .attr('width', xScale.rangeBand())
+  .attr('height', yScale);
 
 chart.selectAll("text").data(data)
   .enter().append("text")
-  .attr('x', xScale)
-  .attr('y', function (d, i) { return yScale(i) + yScale.rangeBand()/2;})
-  .attr('dx', -3)
-  .attr('dy', "0.35em")
-  .attr('text-anchor', 'end')
+  .attr('x', function(d,i) { return xScale(i) + xScale.rangeBand()/2; })
+  .attr('y', function (d, i) { return chartHeight - yScale(d) + 3;})
+  .attr('dy', '0.7em')
+  .attr('text-anchor', 'middle')
   .text(function(d) { return d; })
